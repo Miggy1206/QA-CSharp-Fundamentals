b3ff3b15a2b16b63d3a77d380e23a3495885d286
Miggy1206 <miguelsbarbosa123@gmail.com>
2025-10-20 16:33:28 +0100

    20/10/2025 First Session

---
+++ Functions/Program.cs
@@ -0,0 +1,8 @@
+﻿
+static int Sum(int a, int b){ return a + b;}
+
+Console.WriteLine($"The result of this function is {Sum(1,4)} ");
+
+
+
+
---
+++ Variables/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+class Account
+{
+    private decimal balance;
+
+    public void withdrawal(decimal amount)
+    {
+        this.balance -= amount;
+    }
+    public void deposit(decimal amount)
+    {
+        this.balance += amount;
+    }
+    public decimal getBalance(){ return this.balance; }
+    public void setBalance(decimal balance) { this.balance = balance; }
+}
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        try
+        {
+            Console.WriteLine("Please enter your bank balance:");
+            decimal balance = Convert.ToDecimal(Console.ReadLine());
+            Account account = new Account();
+            account.setBalance(balance);
+            bool valid_choice = false;
+            while (!valid_choice)
+            {
+                try
+                {
+                    Console.WriteLine("Choose an option below:\n[1] Deposit\n[2] Withdrawal\n[3] View Balance\n[]");
+                    int choice = Convert.ToInt16(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            valid_choice = true;
+                            break;
+                        case 2:
+                            valid_choice = true;
+                            break;
+                        case 3:
+                            valid_choice = true;
+                            Console.WriteLine($"Your balance is {account.getBalance()}");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid choice");
+                }
+            }
+
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Invalid Balance");
+        }
+    }
+}
