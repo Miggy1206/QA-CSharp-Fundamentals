9d3ca3123d932b0328d9e19ca63f7dec59713fdf
Miggy1206 <miguelsbarbosa123@gmail.com>
2025-10-20 16:53:46 +0100

    20/10/2025 Task 1, 2 and 3

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 
 class Account
 {
@@ -6,17 +7,35 @@ class Account
 
     public void withdrawal(decimal amount)
     {
-        this.balance -= amount;
-    }
+        if(amount < 0)
+        {
+            throw new ArithmeticException("Amount can not be less than 0.");
+        }
+        else if (amount > this.balance)
+        {
+            throw new ArithmeticException("Amount is bigger than the balance and you do not have an overdraft.");
+        }
+        else
+        {
+            this.balance -= amount;
+        }
+        }
     public void deposit(decimal amount)
     {
-        this.balance += amount;
+        if (amount < 0)
+        {
+            throw new ArithmeticException("Amount can not be less than 0.");
+        }
+        else
+        {
+            this.balance += amount;
+        }
     }
     public decimal getBalance(){ return this.balance; }
     public void setBalance(decimal balance) { this.balance = balance; }
 }
 
-using System;
+
 
 class Program
 {
@@ -29,24 +48,47 @@ class Program
             decimal balance = Convert.ToDecimal(Console.ReadLine());
             Account account = new Account();
             account.setBalance(balance);
-            bool valid_choice = false;
-            while (!valid_choice)
+            bool exit_choice = false;
+            while (!exit_choice)
             {
                 try
                 {
-                    Console.WriteLine("Choose an option below:\n[1] Deposit\n[2] Withdrawal\n[3] View Balance\n[]");
+                    Console.WriteLine("Choose an option below:\n[1] Deposit\n[2] Withdrawal\n[3] View Balance\n[4] Exit\n[]");
                     int choice = Convert.ToInt16(Console.ReadLine());
                     switch (choice)
                     {
                         case 1:
-                            valid_choice = true;
+                            try
+                            {
+                                Console.WriteLine("How much are you wanting to deposit?");
+                                decimal deposit_amount = Convert.ToDecimal(Console.ReadLine());
+                                account.deposit(deposit_amount);
+                            }
+                            catch(ArithmeticException err)
+                            {
+  
+                                Console.WriteLine(err.Message);
+                            }
                             break;
                         case 2:
-                            valid_choice = true;
+                            try
+                            {
+                                Console.WriteLine("How much are you wanting to withdraw?");
+                                decimal withdrawal_amount = Convert.ToDecimal(Console.ReadLine());
+                                account.withdrawal(withdrawal_amount);
+                            }
+                            catch (ArithmeticException err)
+                            {
+                                Console.WriteLine(err.Message);
+                            }
                             break;
                         case 3:
-                            valid_choice = true;
                             Console.WriteLine($"Your balance is {account.getBalance()}");
+                            break;
+                        case 4:
+                            exit_choice = true;
+                            break;
+                        default:
                             break;
                     }
                 }
