c06c1475cef61677265f8c6f46e3d4ee6b4920f1
Miggy1206 <miguelsbarbosa123@gmail.com>
2025-10-21 13:57:18 +0100

    Task Prioritiser completed

---
+++ TaskPrioritiser2/Program.cs
@@ -0,0 +1,142 @@
+﻿using Priority;
+
+namespace Priority
+{
+    public enum TaskPriority
+        {
+            Low,
+            Medium,
+            High,
+            Critical
+    }
+}
+
+class Task
+{
+    private int id;
+    private string description;
+    private DateTime deadline;
+    private TaskPriority priority;
+    private bool status;
+
+    public Task(int id, string description, DateTime deadline, TaskPriority priority, bool status)
+    {
+        this.id = id;
+        this.description = description;
+        this.deadline = deadline;
+        this.priority = priority;
+        this.status = false; // Default status is incomplete
+    }
+
+    public int getId() { return this.id; }
+    public string getDescription() { return this.description; }
+    public DateTime getDeadline() { return this.deadline; }
+    public TaskPriority getPriority() { return this.priority; }
+    public bool getStatus() { return this.status; }
+
+    public void changeStatus()
+    {
+        this.status = !status;
+    }
+
+    override public string ToString()
+    {
+        return $"Task {id}: {this.description}, Deadline: {this.deadline.ToShortDateString()}, Priority: {this.priority}, Status: {(this.status ? "Complete" : "Incomplete")}";
+    }
+
+}
+
+
+
+class Program
+{
+
+    static Task CreateTask(int id_count)
+    {
+        Console.WriteLine("Creating a new task...");
+        Console.WriteLine("Enter task description:");
+        string description = Console.ReadLine();
+        Console.WriteLine("Enter task deadline (yyyy-mm-dd):");
+        DateTime deadline = DateTime.Parse(Console.ReadLine());
+        Console.WriteLine("Enter task priority (Low, Medium, High, Critical):");
+        TaskPriority priority = (TaskPriority)Enum.Parse(typeof(TaskPriority), Console.ReadLine(), true);
+        return new Task(id_count + 1, description, deadline, priority, false);
+    }
+    static void Main(string[] args)
+    {
+        List<Task> tasks = new List<Task>();
+        bool exit_choice = false;
+        while (!exit_choice)
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to the Task Prioritiser!");
+                Console.WriteLine("Choose an option:\n[1] View your tasks\n[2] Create a task\n[3] Mark a task complete/incomplete\n[4] Close");
+                int choice = Convert.ToInt16(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Your Tasks:");
+                        foreach (var task in tasks)
+                        {
+                            if (!task.getStatus())
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                            }
+
+                            Console.WriteLine(task);
+                        }
+                        Console.ReadLine();
+                        Console.ResetColor();
+                        break;
+                    case 2:
+                        if (tasks.Count == 0)
+                        {
+                            tasks.Add(CreateTask(0));
+                            Console.WriteLine("Task created successfully!");
+                            break;
+                        }
+                        tasks.Add(CreateTask(tasks[tasks.Count - 1].getId()));
+                        Console.WriteLine("Task created successfully!");
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the task number to mark as complete:");
+                        int i = 0;
+                        foreach (var task in tasks)
+                        {
+                            i++;
+                            Console.WriteLine($"[{i}] {task}");
+                        }
+                        int taskNumber = Convert.ToInt16(Console.ReadLine());
+                        if (taskNumber > 0 && taskNumber <= tasks.Count)
+                        {
+                            tasks[taskNumber - 1].changeStatus();
+                            Console.WriteLine("Task status updated successfully!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid task number.");
+                        }
+                        break;
+                    case 4:
+                        exit_choice = true;
+                        Console.WriteLine("Exiting Task Prioritiser. Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+            }
+        }
+
+    }
+}
---
+++ enums/Program.cs
@@ -0,0 +1,71 @@
+﻿using Conditionals;
+
+namespace Conditionals
+{
+    public enum  Pole
+    {
+        North,
+        South
+    }
+
+    public enum CapitalCities
+    {
+        London,
+        Paris,
+        Rome,
+        Madrid
+    }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Conditionals.Pole pole = Pole.South;
+        string animal;
+        if (pole == Pole.North)
+        {
+            animal = "Polar Bear";
+        }
+        else
+        {
+            animal = "Penguin";
+        }
+
+        Console.WriteLine($"The animal that lives in the {pole} Pole is the {animal}.");
+    
+    
+        Console.WriteLine("Please enter a capital city (London, Paris, Rome, Madrid):");
+        var city = CapitalCities.Madrid;
+        string countryMessage = "";
+        switch(city)
+        {
+            case CapitalCities.London:
+                countryMessage = "London is the capital of the United Kingdom.";
+                break;
+            case CapitalCities.Paris:
+                countryMessage = "Paris is the capital of France.";
+                break;
+            case CapitalCities.Rome:
+                countryMessage = "Rome is the capital of Italy.";
+                break;
+            case CapitalCities.Madrid:
+                countryMessage = "Madrid is the capital of Spain.";
+                break;
+            default:
+                countryMessage = "Unknown city.";
+                break;
+        }
+
+        Console.WriteLine(countryMessage);
+
+
+
+
+
+
+
+    }
+}
